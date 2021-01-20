7fe3d0cdeadac39cd472cac4b6320ca265d41f8b
Kenan YAGIZ <kenanyagiz@outlook.com>
2021-01-20 12:43:17 +0300

    Add project files.

---
+++ GitHubDeneme/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubDeneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
